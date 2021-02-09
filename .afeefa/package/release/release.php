56a658caac64fb06c0dfcae342d13113fd294337
Kaken Bok <bok@kakenbok.de>
2021-02-09 09:53:05 +0100

    update template

--- .afeefa/package/release/release.php
+++ .afeefa/package/release/release.php
@@ -2,10 +2,9 @@
 
 use Afeefa\Component\Package\Package\Package;
 use Afeefa\Component\Package\ReleaseManager;
-use Webmozart\PathUtil\Path;
 
 return (new ReleaseManager())
     ->packages([
         Package::composer()
-            ->path(Path::join(__DIR__, '..', '..'))
+            ->path(getcwd())
     ]);

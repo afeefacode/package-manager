6d63a7d49872e79bac592bcb8f5e7ec71f6bc6e2
Kaken Bok <bok@kakenbok.de>
2021-03-05 18:02:43 +0100

    update cli-app

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
-use Afeefa\Component\Cli\Application;
+use Afeefa\Component\Cli\Cli;
 use Afeefa\Component\Package\Commands\Release;
 use Afeefa\Component\Package\Commands\Setup;
 
 require_once getcwd() . '/vendor/autoload.php';
 
-(new Application('Afeefa Package Manager'))
+(new Cli('Afeefa Package Manager'))
     ->command('setup', Setup::class, 'Configure an installed package')
     ->command('release', Release::class, 'Release a new version of this package')
     ->run();
